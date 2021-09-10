fcb5432cf531aaea3ced2f6f88ba5afa8589b620
Fabian Nagel <fabian.nagel@live.de>
2021-09-10 16:24:00 +0200

    SchNet: Finish extraction hooks for remaining layers

--- schnet/run.py
+++ schnet/run.py
@@ -7,23 +7,23 @@ from convert import get_converter
 from model import load_model
 
 
-def register_layerwise_hooks(model: OrderedDict):
+def register_layer_hook(layer, layer_name):
+    hook = get_layer_hook(layer_name)
+    layer.register_forward_hook(hook)
 
-    def register_layer_hook(layer, layer_name):
-        hook = get_layer_hook(layer_name)
-        getattr(layer, "register_forward_hook", hook)
 
-    def get_layer_hook(layer_name: str) -> Callable:
-        def hook(model, input, output):
-            layer_outputs[layer_name] = output.detach()
-        return hook
+def get_layer_hook(layer_name: str) -> Callable:
+    def hook(model, input, output):
+        layer_outputs[layer_name] = output.detach()
 
-    # TODO: Manually hook into relevant layers? Or hook into all layers and check later what is needed?
-    layer_member_names = list(dict(model.named_modules()).keys())[1:]
+    return hook
 
-    model.representation.embedding.register_forward_hook(get_layer_hook("representation.embedding"))
-    model.representation.distances.register_forward_hook(get_layer_hook("representation.distances"))
-    model.representation.distance_expansion.register_forward_hook(get_layer_hook("representation.distance_expansion"))
+
+def register_representation_layer_hooks(model: OrderedDict):
+    # layer_member_names = list(dict(model.named_modules()).keys())[1:]
+    register_layer_hook(model.representation.embedding, "representation.embedding")
+    register_layer_hook(model.representation.distances, "representation.distances")
+    register_layer_hook(model.representation.distance_expansion, "representation.distance_expansion")
 
     for interaction_network_idx, interaction_network in enumerate(model.representation.interactions):
         base_name = "representation.interactions.{}.".format(interaction_network_idx)
@@ -31,21 +31,30 @@ def register_layerwise_hooks(model: OrderedDict):
         # cfconv layer
         register_layer_hook(interaction_network.filter_network, base_name + "filter_network.0")
         register_layer_hook(interaction_network.filter_network, base_name + "filter_network.1")
-        register_layer_hook(interaction_network.cutoff_network, "representation.interactions.0.cutoff_network")
+        register_layer_hook(interaction_network.cutoff_network, base_name + "cutoff_network")
         register_layer_hook(interaction_network.cfconv.in2f, base_name + "cfconv.in2f")
         register_layer_hook(interaction_network.cfconv.f2out, base_name + "cfconv.f2out")
 
         # dense output layer
         register_layer_hook(interaction_network.dense, base_name + "dense")
 
-    # TODO: atomwise output network
-    # for output_modules in model.output_modules:
-        # two-layer output MLP
-        # output_modules.out_net[1].out_net[0]
-        # output_modules.out_net[1].out_net[1]
 
-        # output_modules.standardize
-        # output_modules.atom_pool
+def register_output_layer_hooks(model: OrderedDict):
+    for output_module_idx, output_module in enumerate(model.output_modules):
+        base_name = "output_modules.{}.".format(output_module_idx)
+
+        # GetItem()
+        register_layer_hook(output_module.out_net[0], base_name + "0")
+
+        # 2-layer MLP
+        register_layer_hook(output_module.out_net[1].out_net[0], base_name + "1.out_net.0")
+        register_layer_hook(output_module.out_net[1].out_net[1], base_name + "1.out_net.1")
+
+        # ScaleShift()
+        register_layer_hook(output_module.standardize, base_name + "standardize")
+
+        # Aggregate()
+        register_layer_hook(output_module.atom_pool, base_name + "atom_pool")
 
 
 layer_outputs = {}
@@ -53,7 +62,8 @@ cutoff = 5.0  # note: cutoff is also defined in model config!
 device = "cpu"
 
 model = load_model("model_n1.torch", cutoff, device)
-register_layerwise_hooks(model)
+register_representation_layer_hooks(model)
+register_output_layer_hooks(model)
 
 converter = get_converter(cutoff, device)
 atoms = read("geometry.in", format="aims")
