394dc6ca52ccfefe953fdb0c90738734589d7a0c
Fabian Nagel <fabian.nagel@live.de>
2021-10-22 20:44:09 +0200

    interaction block: NL init hack removed. precision increased.

--- tests/test_interaction.py
+++ tests/test_interaction.py
@@ -17,43 +17,16 @@ class InteractionTest(TestCase):
     weights_file = "../schnet/model_n1.torch"
 
     r_cutoff = 5.0
+    rtol = 1e-6
+    atol = 1e-6
 
     def __init__(self, method_name: str):
         super().__init__(method_name)
 
     def setUp(self):
-        _, self.schnet_activations, __ = init.initialize_and_predict_schnet(geometry_file=self.geometry_file, weights_file=self.weights_file, r_cutoff=self.r_cutoff, sort_nl_indices=True)
-        self.schnax_activations = self.initialize_schnax()
-
-    def initialize_schnax(self):
-        """
-        As long as we haven't achieved equal output from the NL layer, use SchNetPack's NL as input for schnax.
-        To make the NL compatible to JAX-MD, we have to:
-
-            (a) sort neighborhood indices in ascending order
-            (b) replace SchNetPack's zero padding with JAX-MD's total atom count padding.
-        """
-
-        schnet_inputs, __, ___ = init.initialize_and_predict_schnet(geometry_file=self.geometry_file, weights_file=self.weights_file, r_cutoff=self.r_cutoff, sort_nl_indices=True)
-        nl = schnet_inputs['_neighbors'][0].cpu().numpy()   # get rid of first dimension, no batches yet.
-
-        mask = nl == 0          # zero valued indices are padding, ...
-        mask[:, 0] = False      # unless they occur within the first column (as the NL is sorted).
-        nl[mask] = nl.shape[0]  # use total atom count as padding.
-
-        # initialize schnax and override NL
-        R, Z, box, neighbors, displacement_fn = init.initialize_schnax(geometry_file=self.geometry_file, r_cutoff=self.r_cutoff)
-        object.__setattr__(neighbors, 'idx', nl)
-
-        layer_outputs, pred = init.predict_schnax(R, Z, displacement_fn, neighbors, self.r_cutoff, weights_file=self.weights_file)
-        return layer_outputs
-
-    def test_distance_expansion(self):
-        """Just a quick sanity check to see if we get approx. equal distance expansions when manually overriding schnax's NL."""
-        # TODO once test_distances.py and test_distance_expansion.py are passing: Remove.
-        schnet_edR, schnax_edR = activation.get_distance_expansion(self.schnet_activations, self.schnax_activations)
-        np.testing.assert_allclose(schnet_edR, schnax_edR, rtol=1e-5, atol=1e-5)
+        _, self.schnet_activations, _ = init.initialize_and_predict_schnet(geometry_file=self.geometry_file, weights_file=self.weights_file, r_cutoff=self.r_cutoff, sort_nl_indices=True)
+        self.schnax_activations, _ = init.initialize_and_predict_schnax(self.geometry_file, self.weights_file, self.r_cutoff, sort_nl_indices=True)
 
     def test_interaction_block(self):
         schnet_interaction, schnax_interaction = activation.get_interaction_output(self.schnet_activations, self.schnax_activations, interaction_block_idx=0)
-        np.testing.assert_allclose(schnet_interaction, schnax_interaction, rtol=1e-5, atol=5 * 1e-5)
+        np.testing.assert_allclose(schnet_interaction, schnax_interaction, rtol=self.rtol, atol=2 * self.atol)
